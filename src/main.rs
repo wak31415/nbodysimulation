218577c0590a5af39725c08b4f105204570ae3b1
OlaviAikas <olavi.aikas@polytechnique.edu>
2021-05-27 11:22:46 +0200

    The cube now moves!

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ extern crate nalgebra as na;
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
 use kiss3d::window::{State, Window};
-use na::{UnitQuaternion, Vector3};
+use na::{UnitQuaternion, Vector3, Translation3};
 
 struct AppState {
     c: SceneNode,
-    rot: UnitQuaternion<f32>,
+    trans: Translation3<f32>,
 }
 
 impl State for AppState {
     fn step(&mut self, _: &mut Window) {
-        self.c.prepend_to_local_rotation(&self.rot)
+        self.c.prepend_to_local_translation(&self.trans);
     }
 }
 
@@ -25,8 +25,9 @@ fn main() {
 
     window.set_light(Light::StickToCamera);
 
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.001);
-    let state = AppState { c, rot };
+    //let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.001);
+    let trans = Translation3::new(-0.2f32, 0f32, 0f32);
+    let state = AppState { c, trans };
 
     window.render_loop(state)
 }
