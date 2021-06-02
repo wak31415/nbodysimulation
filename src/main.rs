679b455a809f72502dff10d7cdfa50f9cdf833a1
Mihails Valtusovs <mihails.valtusovs@polytechnique.edu>
2021-06-02 23:41:34 +0200

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,18 @@ impl State for AppState {
 fn main() {
     let mut window = Window::new("Kiss3d: wasm example");
     let mut c = window.add_cube(1.0, 1.0, 1.0);
-
     c.set_color(1.0, 0.0, 0.0);
+
+    // some kind of fixed reference    
+    let mut s = window.add_cube(10.0, 10.0, 10.0);
+    s.set_color(0.0, 0.0, 1.0);
+    /* try to find how to make the walls of an object
+     * double-sided, this was supposed to be "the universe" */ 
 
     window.set_light(Light::StickToCamera);
 
     //let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.001);
-    let trans = Translation3::new(-0.2f32, 0f32, 0f32);
+    let trans = Translation3::new(-0.02f32, 0f32, 0f32);
     let state = AppState { c, trans };
 
     window.render_loop(state)
