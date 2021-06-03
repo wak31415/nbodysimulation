c9011ba44f334f805b9aa5ecb5ffc41b14aa4ec8
William Koch <william.koch@polytechnique.edu>
2021-06-03 11:52:24 +0200

    Generate Vec of random objects

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate kiss3d;
 extern crate nalgebra as na;
+extern crate rand;
 
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
@@ -10,6 +11,9 @@ use std::time::SystemTime;
 
 // mod thread_manager;
 mod physics;
+
+use rand::thread_rng;
+use rand::Rng;
 
 static N_BODIES: u64 = 2;
 static N_THREADS: u32 = 1;
@@ -49,12 +53,19 @@ fn main() {
 
     println!("Creating {} objects", num_objects);
 
+    let mut objects: Vec<physics::Body> = Vec::new();
+
+    for _i in 0..num_objects {
+        let coord = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
+        let v = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
+        let m = 1000f64 + 1000f64*rand::random::<f64>();
+        objects.push(physics::Body { mass: m, coordinates: coord, velocity: v });
+    }
+
     let mut window = Window::new("Kiss3d: wasm example");
     window.set_light(Light::StickToCamera);
 
-    let A = physics::Body {mass: 50f64, coordinates: Vector3::new(0f64, 0f64, 0f64), velocity: Vector3::new(1f64, 1f64, 1f64), node: window.add_cube(1.0, 1.0, 1.0) };
-    let B = physics::Body {mass: 100f64, coordinates: Vector3::new(10f64, 10f64, 10f64), velocity: Vector3::new(-1f64, -1f64, -1f64), node: window.add_cube(1.0, 1.0, 1.0) };
-    let f = physics::gravitational_force(&A, &B);
+    let f = physics::gravitational_force(&objects[0], &objects[1]);
 
     println!("Force between A and B = {}", f);
     let draw_interval = 1f32/FPS;
--- src/physics.rs
+++ src/physics.rs
@@ -1,13 +1,11 @@
 use nalgebra::{Vector3};
-use kiss3d::scene::SceneNode;
 
 static G : f64  = 0.00000000006674301515151515;
 
 pub struct Body {
     pub mass: f64,
     pub coordinates: Vector3<f64>,
-    pub velocity: Vector3<f64>,
-    pub node: SceneNode
+    pub velocity: Vector3<f64>
 }
 
 pub fn gravitational_force(A: &Body, B: &Body) -> Vector3<f64> {
