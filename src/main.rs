abd5659878d8350b37e0885c1acbc44aef74c44a
OlaviAikas <olavi.aikas@polytechnique.edu>
2021-06-03 12:02:19 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ mod physics;
 use rand::thread_rng;
 use rand::Rng;
 
-static N_BODIES: u64 = 2;
 static N_THREADS: u32 = 1;
 static FPS: f32 = 30f32;
 
@@ -53,27 +52,25 @@ fn main() {
 
     println!("Creating {} objects", num_objects);
 
-    let mut objects: Vec<physics::Body> = Vec::new();
-
-    for _i in 0..num_objects {
+    let mut objects: Vec<physics::Body> = Vec::with_capacity(num_objects);
+    for i in 0..num_objects {
         let coord = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
         let v = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
         let m = 1000f64 + 1000f64*rand::random::<f64>();
-        objects.push(physics::Body { mass: m, coordinates: coord, velocity: v });
+        objects[i] = physics::Body { mass: m, coordinates: coord, velocity: v };
     }
 
     let mut window = Window::new("Kiss3d: wasm example");
     window.set_light(Light::StickToCamera);
 
     let f = physics::gravitational_force(&objects[0], &objects[1]);
-
     println!("Force between A and B = {}", f);
+
     let draw_interval = Duration::from_millis(((1f32/FPS)*1000f32) as u64);
     let mut t_0 = Instant::now();
 
     loop {
-        //If its time to draw, then block all the threads
-        //at this point and draw
+        //If its time to draw, then draw
         let t_1 = Instant::now();
         if t_1.duration_since(t_0) > draw_interval {
             t_0 = t_1;
