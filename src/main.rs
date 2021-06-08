676c424b3b2281c18465e5601f4d93e87b3d5237
William Koch <william.koch@polytechnique.edu>
2021-06-08 18:07:36 +0200

    Read objects from json

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ use std::sync::atomic::AtomicBool;
 use na::{UnitQuaternion, Vector3, Translation3};
 use std::time::{Duration, Instant};
 
-mod thread_manager;
+use serde_json::{Result, Value};
+use std::fs::File;
+use std::io::Read;
+
+// mod thread_manager;
 mod physics;
 
 use rand::thread_rng;
@@ -31,7 +35,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let num_objects: usize = match args.len() {
         1 => {
-            2
+            0
         },
         2 => {
             let arg = &args[1];
@@ -50,19 +54,31 @@ fn main() {
             return;
         }
     };
-
-    println!("Creating {} objects", num_objects);
-
     let mut objects: Vec<physics::Body> = Vec::new();
+
+    if num_objects == 0 {
+        let mut file = File::open("starting_configuration.json").unwrap();
+        let mut data = String::new();
+        file.read_to_string(&mut data).unwrap();
+        let json_objects: Vec<physics::tmpBody> = serde_json::from_str(&data).unwrap();
+    
+        for obj in &json_objects {
+            objects.push(obj.convert());
+            println!("{:?}", obj.convert());
+        }
+    } else {
+        for _i in 0..num_objects {
+            let coord = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
+            let v = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
+            let m = 1000f64 + 1000f64*rand::random::<f64>();
+            objects.push(physics::Body { mass: m, coordinates: coord, velocity: v });
+        }
+    }
+
+    println!("Created {} objects", objects.len());
+
     let mut window = Window::new("Kiss3d: wasm example");
 
-    for _i in 0..num_objects {
-        let coord = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
-        let v = Vector3::new(rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>());
-        let m = 1000f64 + 1000f64*rand::random::<f64>();
-        objects.push(physics::Body { mass: m, coordinates: coord, velocity: v, node: window.add_sphere((m/1000f64) as f32) });
-    }
-    
     window.set_light(Light::StickToCamera);
 
     let f = physics::gravitational_force(&objects[0], &objects[1]);
--- src/physics.rs
+++ src/physics.rs
@@ -1,13 +1,30 @@
 use nalgebra::{Vector3, DMatrix};
-use kiss3d::scene::SceneNode;
+use serde::{Deserialize, Serialize};
 
 static G : f64  = 0.00000000006674301515151515;
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct tmpBody {
+    pub mass: f64,
+    pub coordinates: [f64; 3],
+    pub velocity: [f64; 3]
+}
+
+#[derive(Debug)]
 pub struct Body {
     pub mass: f64,
     pub coordinates: Vector3<f64>,
-    pub velocity: Vector3<f64>,
-    pub node: SceneNode
+    pub velocity: Vector3<f64>
+}
+
+impl tmpBody {
+    pub fn convert(&self) -> Body {
+        Body {
+            mass: self.mass,
+            coordinates: Vector3::new(self.coordinates[0], self.coordinates[1], self.coordinates[2]),
+            velocity: Vector3::new(self.velocity[0], self.velocity[1], self.velocity[2])
+        }
+    }
 }
 
 impl std::fmt::Display for Body {
