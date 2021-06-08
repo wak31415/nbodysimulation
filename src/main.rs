f4b4d508acc3b432f3d322d04c3fc3653e114c40
OlaviAikas <olavi.aikas@polytechnique.edu>
2021-06-08 18:42:19 +0200

    Made thread functions

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ extern crate rand;
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
 use kiss3d::window::{State, Window};
-use std::sync::atomic::AtomicBool;
-use na::{UnitQuaternion, Vector3, Translation3};
+use std::sync::{Arc, Barrier};
+use na::{UnitQuaternion, Vector3, Isometry3, DMatrix};
 use std::time::{Duration, Instant};
 
 mod thread_manager;
@@ -15,8 +15,9 @@ mod physics;
 use rand::thread_rng;
 use rand::Rng;
 
-static N_THREADS: u32 = 1;
+static N_THREADS: usize = 1;
 static FPS: f32 = 30f32;
+static TIME_STEP: f32 = 0.01;
 
 fn help() {
     println!("usage:
@@ -24,6 +25,76 @@ nbodysimulation
     Run the n-body simulation with the default number of bodies (2)
 nbodysimulation <usize>
     Run the n-body simulation with <usize> bodies");
+}
+
+pub fn thread_loop(begin: usize, end: usize, bodies: &mut Vec<physics::Body>, forces: &mut DMatrix<Vector3<f32>>, f_worklist: &Vec<(usize, usize)>, bar: Arc<Barrier>) {
+    assert_eq!(bodies.len(), forces.shape().0);
+    assert_eq!(forces.shape().0, forces.shape().1);
+    let n_bodies = bodies.len();
+    let n_forces = forces.len();
+    loop {
+        //Update forces
+        for f in f_worklist.iter() {
+            let force = physics::gravitational_force(&bodies[f.0], &bodies[f.1]);
+            forces[(f.0, f.1)] = force;
+            forces[(f.1, f.0)] = -force;
+        }
+        bar.wait();
+        // Update positions
+        for i in begin..end {
+            let mut net_force = Vector3::new(0f32,0f32,0f32);
+            for j in 0..n_bodies {
+                net_force += forces[(i, j)];
+            }
+            let acceleration = net_force/bodies[i].mass;
+            bodies[i].velocity += acceleration*TIME_STEP;
+            bodies[i].coordinates += bodies[i].velocity*TIME_STEP;
+        }
+        bar.wait();
+    }
+}
+
+pub fn thread_loop_main(window: &mut Window, body_nodes: &mut Vec<SceneNode>, bodies: &mut Vec<physics::Body>, forces: &mut DMatrix<Vector3<f32>>, f_worklist: &Vec<(usize, usize)>, bar: Arc<Barrier>) {
+    // Main thread by convention does the 1st block because we don't want it to
+    // Be handling the extra at the tail, since its already drawing alone
+    // compute all n^2 forces
+    assert_eq!(bodies.len(), forces.shape().0);
+    assert_eq!(forces.shape().0, forces.shape().1);
+    let n_bodies = bodies.len();
+    let block_size = n_bodies/N_THREADS;
+    let n_forces = forces.len();
+    let draw_interval = Duration::from_millis(((1f32/FPS)*1000f32) as u64);
+    let mut t_0 = Instant::now();
+    loop {
+        //As you can see, the difference is that only the main thread draws
+        //Draw if its time to do so
+        let t_1 = Instant::now();
+        if t_1.duration_since(t_0) > draw_interval {
+            t_0 = t_1;
+            for i in 0..n_bodies {
+                body_nodes[i].set_local_transformation(Isometry3::new(bodies[i].coordinates, Vector3::new(0f32,0f32,0f32)));
+            }
+            window.render();
+        }
+        //Update forces
+        for f in f_worklist.iter() {
+            let force = physics::gravitational_force(&bodies[f.0], &bodies[f.1]);
+            forces[(f.0, f.1)] = force;
+            forces[(f.1, f.0)] = -force;
+        }
+        bar.wait();
+        // Update positions
+        for i in 0..block_size {
+            let mut net_force = Vector3::new(0f32,0f32,0f32);
+            for j in 0..n_bodies {
+                net_force += forces[(i, j)];
+            }
+            let acceleration = net_force/bodies[i].mass;
+            bodies[i].velocity += acceleration*TIME_STEP;
+            bodies[i].coordinates += bodies[i].velocity*TIME_STEP;
+        }
+        bar.wait();
+    }
 }
 
 fn main() {
--- src/physics.rs
+++ src/physics.rs
@@ -1,20 +1,16 @@
 use nalgebra::{Vector3, DMatrix};
 use kiss3d::scene::SceneNode;
 
-static G : f64  = 0.00000000006674301515151515;
+static G : f32  = 0.00000000006674301515151515;
 
 pub struct Body {
-    pub mass: f64,
-    pub coordinates: Vector3<f64>,
-    pub velocity: Vector3<f64>,
+    pub mass: f32,
+    pub coordinates: Vector3<f32>,
+    pub velocity: Vector3<f32>,
     pub node: SceneNode
 }
 
-pub fn gravitational_force(A: &Body, B: &Body) -> Vector3<f64> {
+pub fn gravitational_force(A: &Body, B: &Body) -> Vector3<f32> {
     let f = G*A.mass*B.mass / (A.coordinates - B.coordinates).norm_squared();
     f * (A.coordinates - B.coordinates).normalize()
-}
-
-pub fn update_position(A: &mut Body, forces: &DMatrix<f64>) {
-    // update position of body using a column slice, tbd
 }
